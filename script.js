f77e368070c8fa86bd08270421045965bb586d53
Alexandrodasilva <alexsamdrosilva13@gmail.com>
2021-07-21 14:37:18 -0300

    adcionando no java script o funcionamento para colocar o texto

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 // Seus scripts aqui
 
+var NOME = document.getElementById('string');
+var DATA = document.getElementById('date');
+var IDADE = document.getElementById('idade');
+var SEXOM = document.getElementById('masc');
+var SEXOF = document.getElementById('fem');
+
+var checkbox = document.querySelector('#checkbox-painel-Seg');
+var TEXTO = document.querySelector('.texto');
+
+checkbox.addEventListener('click', function(){
+   
+  if( TEXTO.style.display === 'block'){
+    TEXTO.style.display = 'none';
+  }else{
+    TEXTO.style.display = 'block';
+  }
+
+});
+
+
+
+
+
+
+
+
+function verificar(){
+}
 function datetime() {
     var title = document.getElementById('title');
     var now = new Date();
